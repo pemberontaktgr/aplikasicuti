fcaac88334bdac79b42f81a564e02a06e4753e9b
pemberontaktgr <vickyokaw@gmail.com>
2020-12-12 01:10:57 +0700

    upload hasil uts

--- JAWABAN NO 3 - DESIGN DATABASE.sql
+++ JAWABAN NO 3 - DESIGN DATABASE.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2020 at 05:00 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
+-- Waktu pembuatan: 11 Des 2020 pada 18.07
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cuti_karyawan`
+-- Struktur dari tabel `cuti_karyawan`
 --
 
 CREATE TABLE `cuti_karyawan` (
@@ -33,36 +33,39 @@ CREATE TABLE `cuti_karyawan` (
   `nama` varchar(25) NOT NULL,
   `jabatan` varchar(15) NOT NULL,
   `bagian` varchar(20) NOT NULL,
-  `pengajuan_cuti` int(2) NOT NULL,
-  `sisa_cuti` int(2) NOT NULL
+  `pengajuan_cuti` int(100) DEFAULT NULL,
+  `sisa_cuti` int(2) NOT NULL,
+  `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `cuti_karyawan`
+-- Dumping data untuk tabel `cuti_karyawan`
 --
 
-INSERT INTO `cuti_karyawan` (`id_karyawan`, `NIK`, `nama`, `jabatan`, `bagian`, `pengajuan_cuti`, `sisa_cuti`) VALUES
-(12, '19020027', 'VICKY OKA', 'KEPALA', 'KEUANGAN', 8, 0);
+INSERT INTO `cuti_karyawan` (`id_karyawan`, `NIK`, `nama`, `jabatan`, `bagian`, `pengajuan_cuti`, `sisa_cuti`, `date`) VALUES
+(34, '19020045', 'Vicky oka widiantara', 'Admin', 'Produksi', 2, 10, '2020-12-11'),
+(35, '19020045', 'Vicky', 'Spv', 'Produksi', 10, 10, '2020-12-09'),
+(36, '19020045', 'ayu', 'Manager', 'Marketing', 20, 50, '2020-11-30');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `cuti_karyawan`
+-- Indeks untuk tabel `cuti_karyawan`
 --
 ALTER TABLE `cuti_karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `cuti_karyawan`
+-- AUTO_INCREMENT untuk tabel `cuti_karyawan`
 --
 ALTER TABLE `cuti_karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
